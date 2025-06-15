4b1c53a91e0a3281f2c1b74f9fbe8d343239550f
tommykw <destinys.lotus.1108mn@gmail.com>
2025-06-15 14:27:46 +0900

    Update SHA256 checksum for Dtdrafts formula

--- Formula/dtdrafts.rb
+++ Formula/dtdrafts.rb
@@ -2,7 +2,7 @@ class Dtdrafts < Formula
     desc "CLI tool for searching and managing unpublished articles from dev.to"
     homepage "https://github.com/tommykw/dtdrafts"
     url "https://github.com/tommykw/dtdrafts/releases/download/v0.1.0/dtdrafts-macos.tar.gz"
-    sha256 "PLACEHOLDER_SHA256"
+    sha256 "e78a92f5c01b1e7b8710d1d6c43b8c4dd54af07c4eb68fcdcdfb6ef0878e7219"
     license "MIT"
 
     def install

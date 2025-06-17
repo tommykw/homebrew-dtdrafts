949cce88a4797583de792caf97664275b9726e6e
Kenji Tomita <destinys.lotus.1108mn@gmail.com>
2025-06-18 06:59:12 +0900

    dtdrafts 0.1.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/dtdrafts.rb
+++ Formula/dtdrafts.rb
@@ -1,8 +1,8 @@
 class Dtdrafts < Formula
     desc "CLI tool for searching and managing unpublished articles from dev.to"
     homepage "https://github.com/tommykw/dtdrafts"
-    url "https://github.com/tommykw/dtdrafts/releases/download/v0.1.0/dtdrafts-macos.tar.gz"
-    sha256 "e78a92f5c01b1e7b8710d1d6c43b8c4dd54af07c4eb68fcdcdfb6ef0878e7219"
+    url "https://github.com/tommykw/dtdrafts/releases/download/v0.1.1/dtdrafts-macos.tar.gz"
+    sha256 "925558774e5e1f030e1cd041c533266ab7cc597f95a33978c806d4293c8d2bf6"
     license "MIT"
 
     def install
